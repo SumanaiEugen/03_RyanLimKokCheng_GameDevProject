a406752b56c5579225fb8cdc3f391d1afd80bf24
SumanaiEugen <48399145+SumanaiEugen@users.noreply.github.com>
2021-02-03 16:45:44 +0800

    Main

---
+++ 03_RyanLimKokCheng_GameDevProject/Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject PlayerGO;
+
+    public Transform playerTransform;
+
+    public bool rotateAroundPlayer = true;
+    public bool LookatPlayer = false;
+
+    public float rotationspeed = 5f;
+
+    Vector3 posoffset = new Vector3(0f, 1.5f, -5f);
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, PlayerGO.transform.position + posoffset, 0.1f);
+
+        if (rotateAroundPlayer)
+        {
+            Quaternion CamTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationspeed, Vector3.up);
+            
+            posoffset = CamTurnAngle * posoffset;
+        }
+
+        if(LookatPlayer || rotateAroundPlayer)
+        {
+            transform.LookAt(playerTransform);
+        }
+    }
+}
---
+++ 03_RyanLimKokCheng_GameDevProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float rotateSpeed = 200f;
+    public float walkSpeed;
+    public Animator PlayerAni;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+    void Movement()
+    {
+
+        
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * walkSpeed);
+            PlayerAni.SetBool("IsWalking", true);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, -rotateSpeed * Time.deltaTime, 0));
+            PlayerAni.SetBool("IsWalking", true);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * walkSpeed);
+            PlayerAni.SetBool("IsWalkingB", true);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, rotateSpeed * Time.deltaTime, 0));
+            PlayerAni.SetBool("IsWalking", true);
+        }
+
+        //key released
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.W))
+        {
+            PlayerAni.SetBool("IsWalking", false);
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            PlayerAni.SetBool("IsWalkingB", false);
+        }
+        
+    }
+}
