cc2dafd630e08c1ef26f82f9ccc738187076de35
SumanaiEugen <48399145+SumanaiEugen@users.noreply.github.com>
2021-02-04 13:29:43 +0800

    updated

--- 03_RyanLimKokCheng_GameDevProject/Assets/Scripts/PlayerController.cs
+++ 03_RyanLimKokCheng_GameDevProject/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     float rotateSpeed = 200f;
+
+    public float runSpeed;
     public float walkSpeed;
+    float jumpcounter = 0;
+
+    public Rigidbody PlayerRb;
     public Animator PlayerAni;
     // Start is called before the first frame update
     void Start()
@@ -17,11 +22,14 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Movement();
+        Attack();
+        Jump();
     }
+
     void Movement()
     {
 
-        
+        //walking state
         if(Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward * Time.deltaTime * walkSpeed);
@@ -46,16 +54,61 @@ public class PlayerController : MonoBehaviour
             PlayerAni.SetBool("IsWalking", true);
         }
 
+        //running state
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * runSpeed);
+            PlayerAni.SetBool("IsRunning", true);
+        }
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Rotate(new Vector3(0, rotateSpeed * Time.deltaTime, 0));
+            PlayerAni.SetBool("IsRunning", true);
+        }
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Rotate(new Vector3(0, -rotateSpeed * Time.deltaTime, 0));
+            PlayerAni.SetBool("IsRunning", true);
+        }
+
         //key released
         if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.W))
         {
             PlayerAni.SetBool("IsWalking", false);
         }
-
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            PlayerAni.SetBool("IsRunning", false);
+        }
         if (Input.GetKeyUp(KeyCode.S))
         {
             PlayerAni.SetBool("IsWalkingB", false);
         }
         
+    }
+    void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && jumpcounter == 0)
+        {
+            PlayerRb.AddForce(Vector3.up * 3, ForceMode.Impulse);
+            PlayerAni.SetTrigger("Jump");
+            jumpcounter++;
+        }
+    }
+
+    void Attack()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            PlayerAni.SetTrigger("Attack");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            jumpcounter = 0;
+        }
     }
 }
